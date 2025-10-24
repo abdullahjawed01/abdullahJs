a2846a5badabe33521ad608c5adf9fe6ce57be9b
abdullahjawed01 <abdullahjawed2021@gmail.com>
2025-10-24 20:23:29 +0530

    added loops

--- 26.js
+++ 26.js
@@ -11,4 +11,23 @@ do{
 // Runs the code atleast once, then checks the condition
 
 
+// Table using for
 
+for (let a = 1; a<=100 ;a++) {
+    console.log(`100 X ${a} = ${a*100}`);
+}
+
+// table using while
+let a = 1;
+while (a<=100) {
+    console.log(`100 X ${a} = ${a*100}`);
+    a++;
+}
+
+// table using do while
+
+let a1 = 1;
+do {
+    console.log(`100 X ${a1} = ${a1*100}`);
+    a1++;
+} while (a<=100);
---
+++ 27.js
@@ -0,0 +1,28 @@
+// Break and continue
+
+
+for (let i = 0; i <=10; i++) {
+      console.log(i);
+    if(i == 5) {
+        console.log("I have stopped");    
+        break;
+
+    }
+
+  
+}
+
+
+
+
+// continue
+
+for (let a = 0; a <=10; a++) {
+    if (a == 5) {
+        console.log("checkpoint");
+        continue;
+    }
+    console.log(a);
+}
+
+
