318713b1bd7e25c493e1b55303e0ff336e3319b7
Abdullah Jawed <abdullahjawed2021@gmail.com>
2025-12-06 20:11:07 +0530

    Update 1.js

--- 1.js
+++ 1.js
@@ -4,7 +4,7 @@ console.log("Abdullah Jawed");
 console.log("Patna Bihar");
 console.log("Code for india Foundation")
 console.log("class of B25")
-
+console.log("class of the social dilemma ")
 // if you want multiline comments
 /* 
 this is multi line comments
